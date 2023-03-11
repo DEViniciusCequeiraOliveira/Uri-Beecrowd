56d481f4dddbb646f6e528c7c1b2223c02e4faa3
Vinicius Cerqueira Oliveira <dev.vinicius.c.oliveira@gmail.com>
2023-03-11 14:11:15 -0300

    primeiro commit

---
+++ bee1003/bee1003.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int soma;
+    int valor1;
+    int valor2;
+
+    cin >> valor1;
+    cin >> valor2;
+
+    soma = valor1 + valor2;
+
+    cout << "SOMA = " << soma <<"\n";
+
+    system("pause");
+    return 0;
+}
---
+++ bee1004/bee1004.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int valor1;
+    int valor2;
+
+    cin >> valor1;
+    cin >> valor2;
+
+    int prod = valor1 * valor2;
+
+    cout << "PROD = " << prod << "\n";
+
+    system("pause");
+    return 0;
+}
