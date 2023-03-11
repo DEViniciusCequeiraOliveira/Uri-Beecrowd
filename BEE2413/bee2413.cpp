213cf664907e89ff550a20fd332f6bdc2d02ce1b
Vinicius Cerqueira Oliveira <dev.vinicius.c.oliveira@gmail.com>
2023-03-11 17:04:19 -0300

    BEE2413

---
+++ BEE2413/bee2413.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int clickTerceiroLink;
+
+    cin >> clickTerceiroLink;
+
+    cout << clickTerceiroLink * 4 << endl;
+
+    return 0;
+}
