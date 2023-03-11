938089ed69c5fa0227122eea54e9d9f34be965cf
Vinicius Cerqueira Oliveira <dev.vinicius.c.oliveira@gmail.com>
2023-03-11 17:24:24 -0300

    arrumando as tabelas e BEE2374

---
+++ BEE2374/bee2374.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int presaoDesejada, pressaoLida;
+
+    cin >> presaoDesejada;
+    cin >> pressaoLida;
+
+    cout << presaoDesejada - pressaoLida << endl;
+
+    return 0;
+}
